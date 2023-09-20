b3ac07904cd70c56f9a40cfc480178bf964f6a37
David Ambrozio <davidambroziodev@gmail.com>
2023-09-20 16:43:48 -0300

    fix time event

--- app.py
+++ app.py
@@ -12,12 +12,15 @@ config = dotenv_values('.env')
 
 driver.get(r"https://twitter.com/i/flow/login")
 
-time.sleep(2)
+while(len(driver.find_elements(By.NAME, 'text'))) == 0:
+    time.sleep(1)
+
 
 driver.find_element(By.NAME, 'text').send_keys(config['TWITTER-USER'])
 driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]').click()
 
-time.sleep(2)
+while(len(driver.find_elements(By.NAME, 'password'))) == 0:
+    time.sleep(1)
 
 driver.find_element(By.NAME, 'password').send_keys(config['TWITTER-PASSWORD'])
 driver.find_element(By.XPATH, '/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div').click()
@@ -26,7 +29,8 @@ time.sleep(2)
 
 driver.get(config['TWITTER-HASHTAG_URL'])
 
-time.sleep(2)
+while(len(driver.find_elements(By.XPATH, '//div[@data-testid="like"]'))) == 0:
+    time.sleep(1)
 
 like_buttons = driver.find_elements(By.XPATH, '//div[@data-testid="like"]')
 for like_button in like_buttons:
